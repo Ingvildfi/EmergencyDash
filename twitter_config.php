087827734c47d6ef605a3796b36847cdcd81b087
Ingvildfi <fiskerstrand94@hotmail.com>
2017-12-06 17:11:49 +0100

    Delete twitter_config.php

--- twitter_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Tweet to Map configuration
-    $twitter_app_consumerKey = 'cKwhWAoENgtld43s0CaljXDh5';
-    $twitter_app_consumerSecret = 'SoCKMS4Np9fI7ZMzzS4KJYuGpt3dj6MdJaNxxCS4wBpMeCWLVD';
-
-    $twitter_app_accessToken = '34640895-LemS8RPOvyCFxN6WFgVayNb7AA3Ne0qo6lndVNZMs';
-    $twitter_app_accessSecret = 'a2SqIcJu522ZEZfH4lrUTyuFEl5qudVGzLmlpNK8MNuh9';
-?>
