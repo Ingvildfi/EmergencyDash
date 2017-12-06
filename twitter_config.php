87c3be618eb864ec535af844ddcf436157016b18
Ingvildfi <fiskerstrand94@hotmail.com>
2017-12-06 15:06:54 +0100

    Update twitter_config.php

--- twitter_config.php
+++ twitter_config.php
@@ -1,8 +1,8 @@
 <?php
     // Tweet to Map configuration
-    $twitter_app_consumerKey = '8YHL8VHBWZFJ84ewvx8qUA';
-    $twitter_app_consumerSecret = 'GPzLHyUWiezdWasEkLAAKOaSaou7PQn6m7z0vSNi1kw';
+    $twitter_app_consumerKey = 'cKwhWAoENgtld43s0CaljXDh5';
+    $twitter_app_consumerSecret = 'SoCKMS4Np9fI7ZMzzS4KJYuGpt3dj6MdJaNxxCS4wBpMeCWLVD';
 
-    $twitter_app_accessToken = '14405382-vBJ1KnwAzqcv7OHfQw0Mrze3QiZ1MgOzOYFFK1g8G';
-    $twitter_app_accessSecret = 'SioVTAHvZLp1TURg7ylqFcHgl2aRMSedypaYvdMe0I';
+    $twitter_app_accessToken = '34640895-LemS8RPOvyCFxN6WFgVayNb7AA3Ne0qo6lndVNZMs';
+    $twitter_app_accessSecret = 'a2SqIcJu522ZEZfH4lrUTyuFEl5qudVGzLmlpNK8MNuh9';
 ?>
